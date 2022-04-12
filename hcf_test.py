ee6e63aa89da432b939b9f02986062b7f1116d2a
Mine-Ranger <75626127+Mine-Ranger@users.noreply.github.com>
2022-04-12 21:27:47 +0530

    included datatype in list hcf test

--- hcf_test.py
+++ hcf_test.py
@@ -7,6 +7,8 @@ class hcftest(unittest.TestCase):
     def test_values(self):
         self.assertEqual(hcf.list_hcf([10, 15, 20]), 5)
         self.assertEqual(hcf.list_hcf([10, 20, 40]), 10)
+    def test_datatype(self):
+        self.assertIsInstance(hcf.list_hcf([10, 15, 20]),int)
 
 
 if __name__ == "__main__":
